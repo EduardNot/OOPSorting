a97206ff5a648fc4254eda3d42acecb50b48a9a0
kaarelkaasla <kaarel.est@gmail.com>
2019-03-31 22:48:02 +0300

    Initial bubble sort implementation and some minor testing

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int[] list = {1, 9, 4, 6, 5, -4};
         System.out.println(Arrays.toString(InsertionSort.insertionSort(list)));
+        System.out.println(Arrays.toString(BubbleSort.bubbleSort(list)));
     }
 }
